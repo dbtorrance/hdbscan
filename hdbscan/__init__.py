6fcad701d5c251f8aa753029d3cf4fa58a09e7c5
Britt Torrance <datorran@amazon.com>
2021-03-05 14:52:21 -0800

    including the flat cluster functions in __init__

--- hdbscan/__init__.py
+++ hdbscan/__init__.py
@@ -1,4 +1,5 @@
 from .hdbscan_ import HDBSCAN, hdbscan
+from .flat import HDBSCAN_flat, approximate_predict_flat
 from .robust_single_linkage_ import RobustSingleLinkage, robust_single_linkage
 from .validity import validity_index
 from .prediction import (approximate_predict,
